a7541d93e9e9f89375142bbb45cfa76d17024d45
runningsnail <496668219@qq.com>
2019-07-18 16:38:54 +0800

    修改错误

--- Example/Pods/Target Support Files/LLCommonTool/LLCommonTool-umbrella.h
+++ Example/Pods/Target Support Files/LLCommonTool/LLCommonTool-umbrella.h
@@ -17,21 +17,6 @@
 #import "NSString+Encode.h"
 #import "NSString+Rounding.h"
 #import "NSString+Size.h"
-#import "NSObject+MGModel.h"
-#import "UIColor+MGColor.h"
-#import "UIFont+MGFont.h"
-#import "UIImageView+MGWebImage.h"
-#import "UIAlertController+Init.h"
-#import "UICollectionView+Register.h"
-#import "UIImage+Size.h"
-#import "UILabel+AttributedText.h"
-#import "UILabel+Init.h"
-#import "UILabel+Space.h"
-#import "UILabel+TextSize.h"
-#import "UITableView+Register.h"
-#import "UITextField+Check.h"
-#import "UIView+AddSubview.h"
-#import "UIView+Frame.h"
 
 FOUNDATION_EXPORT double LLCommonToolVersionNumber;
 FOUNDATION_EXPORT const unsigned char LLCommonToolVersionString[];
